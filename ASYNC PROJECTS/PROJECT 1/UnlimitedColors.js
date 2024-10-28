1424e931ce5968a80cb9277a9d86f607c351a42d
Kirti Goel <kirtigoyal5104@gmail.com>
2024-10-28 14:22:33 +0530

    added two projects

---
+++ ASYNC PROJECTS/PROJECT 1/UnlimitedColors.js
@@ -0,0 +1,23 @@
+//generate a random color
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+let x
+let startChangingColor = function(){
+    x  = setInterval(changeBgColor, 1000);
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+let stopChangingColor = function(){
+    clearInterval(x);
+}
+document.getElementById('Start').addEventListener('click', startChangingColor)
+document.getElementById('Stop').addEventListener('click', stopChangingColor)
